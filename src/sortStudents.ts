4160703c2129a61929e19bb76c1fb225086a9425
Pamela Szczerbiak <pamela.szczerbiak@gmail.com>
2025-04-07 14:45:58 +0200

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,77 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'Name',
+  Surname = 'Surname',
+  Age = 'Age',
+  Married = 'Married',
+  AverageGrade = 'AverageGrade',
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const studentsCopy = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  const compare = (a: Student, b: Student): number => {
+    let aValue: string | number | boolean;
+    let bValue: string | number | boolean;
+
+    switch (sortBy) {
+      case SortType.Name:
+        aValue = a.name;
+        bValue = b.name;
+        break;
+      case SortType.Surname:
+        aValue = a.surname;
+        bValue = b.surname;
+        break;
+      case SortType.Age:
+        aValue = a.age;
+        bValue = b.age;
+        break;
+      case SortType.Married:
+        aValue = a.married;
+        bValue = b.married;
+        break;
+
+      case SortType.AverageGrade: {
+        const avg = (
+          grades: number[],
+        ): number => grades
+          .reduce((sum, g) => sum + g, 0) / grades.length;
+
+        aValue = avg(a.grades);
+        bValue = avg(b.grades);
+        break;
+      }
+
+      default:
+        throw new Error(`Nieznany typ sortowania: ${sortBy}`);
+    }
+
+    if (aValue < bValue) {
+      return order === 'asc' ? -1 : 1;
+    }
+
+    if (aValue > bValue) {
+      return order === 'asc' ? 1 : -1;
+    }
+
+    return 0;
+  };
+
+  return studentsCopy.sort(compare);
 }
